e2a2d6f00d9567ee8326b1b22fed305ae7f89ee7
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-10-17 23:40:10 +0300

    feat: enhancing the cli experience, adding error handling and 404 handle middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import chalk from "chalk";
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config();
@@ -22,7 +23,9 @@ app.use((req: Request, res: Response, next) => {
       // *calculate the duration of the request
       const duration = new Date().getTime() - time.getTime();
       console.log(
-        `${req.method} ${req.url} - ${res.statusCode} - ${duration}ms`
+        `${chalk.blue(req.method)} ${chalk.green(req.url)} - ${chalk.yellow(
+          res.statusCode.toString()
+        )} - ${chalk.magenta(duration + "ms")}`
       );
     });
     next();
@@ -45,4 +48,57 @@ app.get("/", (_, res: Response) => {
   res.send(serverStatus);
 });
 
-app.listen(port);
+// define a route handler for the health check
+// returns the uptime of the server
+app.get("/api/v1/health", async (_, res: Response) => {
+  const healthCheck = {
+    upTime: process.uptime(),
+    status: "Ok",
+    date: new Date(),
+  };
+
+  try {
+    res.send(healthCheck);
+  } catch (error) {
+    healthCheck.status = "Error";
+    res.status(503).send(healthCheck);
+  }
+});
+
+// create error handler middleware
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  console.error(chalk.bgRed(err.message));
+  res.status(req.statusCode || 500).json({
+    status: "error",
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+});
+
+// define a route handler for the notfound
+app.use((_, res: Response) => {
+  res.status(404).send("Not found");
+});
+
+app.listen(port, () => {
+  console.log(
+    chalk.black.bgWhiteBright(`
+ _____ ______   ________  ________   _______       ___    ___ ___       __   ___  ________  _______      
+|\\   _ \\  _   \\|\\   __  \\|\\   ___  \\|\\  ___ \\     |\\  \\  /  /|\\  \\     |\\  \\|\\  \\|\\   ____\\|\\  ___ \\     
+\\ \\  \\\\\\__\\ \\  \\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\   __/|    \\ \\  \\/  / \\ \\  \\    \\ \\  \\ \\  \\ \\  \\___|\\ \\   __/|    
+ \\ \\  \\\\|__| \\  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\_|/__   \\ \\    / / \\ \\  \\  __\\ \\  \\ \\  \\ \\_____  \\ \\  \\_|/__  
+  \\ \\  \\    \\ \\  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\_|\\ \\   \\/  /  /   \\ \\  \\|\\__\\_\\  \\ \\  \\|____|\\  \\ \\  \\_|\\ \\ 
+   \\ \\__\\    \\ \\__\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\__/  / /      \\ \\____________\\ \\__\\____\\_\\  \\ \\_______\\
+    \\|__|     \\|__|\\|_______|\\|__| \\|__|\\|_______|\\___/ /        \\|____________|\\|__|\\_________\\|_______|
+                                                 \\|___|/                            \\|_________|         `)
+  );
+
+  console.log(
+    chalk.green("App is running in ") +
+      chalk.yellow(process.env.NODE_ENV) +
+      chalk.magenta(" mode")
+  );
+  console.log(
+    chalk.green(`Server started at `) + chalk.bgGrey(`http://localhost:${port}`)
+  );
+});
